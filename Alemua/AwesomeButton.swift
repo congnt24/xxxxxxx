982ad6bf32b77a8110bb7744d1592deb18f04301
Congnt <congnt24@gmail.com>
2017-09-04 18:13:42 +0700

    fix line

--- Alemua/AwesomeButton.swift
+++ Alemua/AwesomeButton.swift
@@ -16,7 +16,7 @@ class AwesomeButton: UIButton {
         didSet {
             let border = CALayer()
             border.borderColor = UIColor.lightGray.cgColor
-            border.frame = CGRect(x: 0, y: frame.size.height - bottomLineWidth, width: frame.size.width, height: frame.size.height)
+            border.frame = CGRect(x: 0, y: frame.size.height - bottomLineWidth, width: UIScreen.main.bounds.width, height: frame.size.height)
             border.borderWidth = bottomLineWidth
             layer.addSublayer(border)
             layer.masksToBounds = true
@@ -26,7 +26,7 @@ class AwesomeButton: UIButton {
         didSet {
             let border = CALayer()
             border.borderColor = bottomLineColor.cgColor
-            border.frame = CGRect(x: 0, y: frame.size.height - bottomLineWidth, width: frame.size.width, height: frame.size.height)
+            border.frame = CGRect(x: 0, y: frame.size.height - bottomLineWidth, width: UIScreen.main.bounds.width, height: frame.size.height)
             border.borderWidth = bottomLineWidth
             layer.addSublayer(border)
             layer.masksToBounds = true
